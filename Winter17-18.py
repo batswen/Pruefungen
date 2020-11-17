1e4868d795d455869b0057a1f583c2bb3da4b74f
batswen <batswen@gmail.com>
2020-11-17 17:53:56 +0100

    Kommentare hinzugefügt

--- Winter17-18.py
+++ Winter17-18.py
@@ -6,12 +6,17 @@
 
 def freie_sitze(anzahl_sitze):
     for reihe in range(7):
+        # Anzahl Sitze nebeneinander
         anzahl = 0
         for sitz in range(30):
             if kino[reihe][sitz]:
+                # Einen freien Sitz gefunden
                 anzahl = anzahl + 1
                 if anzahl == anzahl_sitze:
+                    # Die benötigte Anzahl wurde erreicht
                     return (reihe + 1) * 100 + anzahl + 1
             else:
+                # Kein freier Sitzplatz: zurück zum Start
                 anzahl = 0
+    # Nix gefunden
     return 0

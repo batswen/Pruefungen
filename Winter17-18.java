bcfc724c77c5f14886db0e128c1935e1b772bd3a
batswen <batswen@gmail.com>
2020-11-17 17:53:00 +0100

    Kommentare hinzugefügt

--- Winter17-18.java
+++ Winter17-18.java
@@ -1,4 +1,4 @@
-// Lösung der Aufgabe "Freie Kinositze auf dem Kreuzfahrtschiff"
+// Lösung der Aufgabe "Freie nebeneinander liegende Kinositze auf dem Kreuzfahrtschiff"
 //
 // 7 Sitzreihen, 30 Plätze pro Reihe
 // Sitzreihe 1: Sitznummern 101 bis 130
@@ -6,17 +6,22 @@
 
 public int freieSitze(int anzahlSitze) {
     for (int reihe = 0; reihe < 7; reihe++) {
+        // Anzahl Sitze nebeneinander
         int anzahl = 0;
         for (int sitz = 0; sitz < 30; sitz++) {
             if (kino[reihe][sitz]) {
+                // Einen freien Sitz gefunden
                 anzahl++;
                 if (anzahl == anzahlSitze) {
+                    // Die benötigte Anzahl wurde erreicht
                     return (reihe + 1) * 100 + anzahl + 1;
                 }
             } else {
+                // Kein freier Sitzplatz: zurück zum Start
                 anzahl = 0;
             }
         }
     }
+    // Nix gefunden
     return 0;
 }
